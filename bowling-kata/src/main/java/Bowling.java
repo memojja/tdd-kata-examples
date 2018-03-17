e0b1f05bbc7d6a92812159d1fc4914508bba900e
mehmet <memojja1907@gmail.com>
2018-03-17 16:56:34 +0300

    bowling kata step2  - bowlingwhen all rolls are one then score is twenty

--- bowling-kata/src/main/java/Bowling.java
+++ bowling-kata/src/main/java/Bowling.java
@@ -1,9 +1,11 @@
 public class Bowling {
+    private int score;
     public int roll(int i) {
+        score+=i;
         return i;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
--- bowling-kata/src/test/java/BowlingTest.java
+++ bowling-kata/src/test/java/BowlingTest.java
@@ -12,4 +12,13 @@ public class BowlingTest {
 
         assertEquals(0,bowling.score());
     }
+
+    @Test
+    public void when_all_rools_are_one_then_score_is_twenty(){
+        Bowling bowling = new Bowling();
+        for (int i = 0; i < 20; i++) {
+            bowling.roll(1);
+        }
+        assertEquals(20,bowling.score());
+    }
 }
