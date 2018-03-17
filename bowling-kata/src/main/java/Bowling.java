962eb9c1e99901d0bfd17c9585ba3223e0984b71
mehmet <memojja1907@gmail.com>
2018-03-17 18:03:27 +0300

    bowling kata - some refactoring

--- bowling-kata/src/main/java/Bowling.java
+++ bowling-kata/src/main/java/Bowling.java
@@ -5,28 +5,30 @@ public class Bowling {
 
     public void roll(int pins) {
         frames[frame]=pins;
-        if ((pins == 10)) {
-            frame += 2;
-        } else {
-            frame++;
-        }
-
+        if ((pins == 10)) frame += 2;
+        else frame++;
     }
 
     public int score() {
         int score=0;
         for (int i = 0; i < frames.length; i+=2) {
-            if (isStrike(frames[i])){
-                score+=frames[i]+frames[i+2]+frames[i+3];
-            }
-            else if (isSpare(i)){
-                score+=frames[i]+frames[i+1]+frames[i+2];
-            }
-            else {
-                score+=frames[i]+frames[i+1];
-            }
+            if (isStrike(frames[i])) score+= getStrikeScore(i);
+            else if (isSpare(i)) score+= getSpareBonus(i);
+            else score+= getFrameScore(i);
         }
         return score;
+    }
+
+    private int getStrikeScore(int i) {
+        return frames[i]+frames[i+2]+frames[i+3];
+    }
+
+    private int getFrameScore(int i) {
+        return frames[i]+frames[i+1];
+    }
+
+    private int getSpareBonus(int i) {
+        return frames[i]+frames[i+1]+frames[i+2];
     }
 
     private boolean isStrike(int frame) {
--- bowling-kata/src/test/java/BowlingTest.java
+++ bowling-kata/src/test/java/BowlingTest.java
@@ -1,6 +1,10 @@
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import static org.junit.Assert.*;
 
 public class BowlingTest {
@@ -9,9 +13,7 @@ public class BowlingTest {
 
     @Test
     public void when_all_rolls_are_zero_then_score_is_zero(){
-
         rollMany(0,20);
-
         assertEquals(0,bowling.score());
     }
 
@@ -21,16 +23,9 @@ public class BowlingTest {
         assertEquals(20,bowling.score());
     }
 
-    private void rollMany(int pins,int count) {
-        for (int i = 0; i < count; i++) {
-            bowling.roll(pins);
-        }
-    }
-
     @Test
     public void when_rolls_are_5_5_3_then_score_must_be_16(){
-        bowling.roll(5);
-        bowling.roll(5);
+        rollSpare();
         bowling.roll(3);
         for (int i = 0; i < 17; i++) {
             bowling.roll(0);
@@ -38,13 +33,28 @@ public class BowlingTest {
         assertEquals(16,bowling.score());
     }
 
+
     @Test
     public void when_rolls_are_10_3_3_then_Score_must_be_22(){
-        bowling.roll(10);
+        rollStrike();
         bowling.roll(3);
         bowling.roll(3);
         rollMany(0,16);
         assertEquals(22,bowling.score());
+    }
+
+    private void rollMany(int pins,int count) {
+        for (int i = 0; i < count; i++) {
+            bowling.roll(pins);
+        }
+    }
+    private void rollStrike() {
+        bowling.roll(10);
+    }
+
+    private void rollSpare() {
+        bowling.roll(5);
+        bowling.roll(5);
     }
 
 }
